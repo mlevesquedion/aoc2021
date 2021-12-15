3759ffa42a5c5e3c244e64699fd55e2138a1438a
Michaël Lévesque-Dion <mlevesquedion@google.com>
2021-12-14 20:52:41 -0500

    Day 14

---
+++ d14.clj
@@ -0,0 +1,35 @@
+(def lines (clojure.string/split-lines (slurp "d14.txt")))
+
+(def template (first lines))
+
+(def initial-pair-counts
+  (->> (partition 2 1 template)
+       (reduce #(update %1 %2 (fnil inc 0)) {})))
+
+(def insertion-rules
+  (->> (drop 2 lines)
+       (map #(clojure.string/split % #" -> "))
+       (map (fn [[[x y] [z]]] {[x y] z}))
+       (apply merge)))
+
+(defn step [pair-counts]
+  (->> (map (fn [[[x y :as p] c]]
+              (let [z (insertion-rules p)]
+                {[x z] c [z y] c})) pair-counts)
+       (apply merge-with +)))
+
+(defn ->item-counts [pair-counts]
+  (->> (map (fn [[[a _] c]] {a c}) pair-counts)
+       (apply merge-with +)
+       (#(update % (last template) (fnil inc 0)))))
+
+(defn solve [n]
+  (let [pair-counts (nth (iterate step initial-pair-counts) n)
+        counts (map val (->item-counts pair-counts))]
+    (- (apply max counts) (apply min counts))))
+
+; part 1
+(solve 10)
+
+; part 2
+(solve 40)
